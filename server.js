d18313adccf5797f6ccf6408d1d5d3e8c3c49e0e
Alba Molina <89818140+albavmolina@users.noreply.github.com>
2021-10-03 19:30:06 -0400

    Add files via upload
    "budgtr submit lab"

--- server.js
+++ server.js
@@ -1,59 +1,47 @@
-const express = require('express');
-const budgets= require('./models/budgets);
-const logger = require('morgan');
+const express = require("express");
+const budgets = require("./models/budget.js")
 
-// initialize the express app
+//initialize app
 const app = express();
 
-// configure application settings
-require('dotenv').config();
+//Mount middleware
+app.use(express.urlencoded({ extended: false})); // This is the only middleware we have been using up to date!!!!!!!!!!!!!! this middleware takes shit and turns it into objects 
 
-// mount middleware - app.use()
-// this turns incoming json data into req.body
-app.use(express.json());
-app.use(logger('dev'));
+app.get('/', (req, res) => {
+    res.send('welcome to our budgeting app');
+})
 
-
-// Index Route - See an index of all the things!
-// GET -> /resource_name_pluralized
-app.get('/budgets', (req, res) => {
-    res.render('bugets_index.ejs', {
-    allBudgets: budgets,
-
-});
+//Index
+app.get("/budgets", (req, res) => {
+    res.render("index.ejs", {
+        budget: budgets,
+    });
 });
 
-// New Route - Send the user to a dedicated page with a form
-app.get('/budgets/new', (req, res) => {
-    res.render('budgets_new.ejs');
-    });
+//NEW 
+app.get("/budgets/new", (req, res) => {
+    res.render("new.ejs");
+})
 
-    // Create Route - Create a resource
-// POST -> /resource_name_pluralized
-
-// Create Route - Create a resource
-// POST -> /resource_name_pluralized
-
-app.post('/budgets', (req, res) => {
-
-    budgets.push(req.body);
-    res.redirect('/budgets')
-    }); 
-// Show Route - Show me one specific thing
-// GET -> /resource_name_pluralized/:resource_identifier
-app.get('/budgets/:budgets_index'), (req, res) => {
-    res.render('budgets_show.ejs'), {
-    budget: budgets[req.params.indexOfFruitsArray]
-    });
-    });
-
-// tell the app to listen on port 3000 for requests from the client
-
-const port = process.env.PORT;
-
-// process.env is an object that lives in the node environment
-// we use it to store environment variable
-
-app.listen(port, () => {
-console.log(`Express is listening on port:${port}`);
+//POST
+app.post("/budgets", (req, res) => {
+    console.log(req.body);
+    const budget = {
+            date: req.body.date, 
+            name: req.body.name,
+            from: req.body.from,
+            amount:req.body.amount,
+            tags: req.body.tags
+        }; 
+    budgets.push(budget)
+    res.redirect("/budgets");
 });
+
+//Show route
+app.get("/budgets/:index", (req, res) => {
+    res.render("show.ejs", { 
+        budget: budgets[req.params.index]    
+    })   
+});
+
+app.listen(3000);
